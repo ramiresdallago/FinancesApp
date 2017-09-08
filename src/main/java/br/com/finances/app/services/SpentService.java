24b0eebd9a1719937172f4c104bff2caa0192a3d
Ramires P Dallago <ramirespatriciodallago@gmail.com>
2017-09-08 16:08:22 -0300

    Adicionando APIs basicas

---
+++ src/main/java/br/com/finances/app/services/SpentService.java
@@ -0,0 +1,47 @@
+package br.com.finances.app.services;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/spent")
+public class SpentService {
+
+	@POST
+	@Path("/resgister")
+	public Response registerSpent() {
+		return null;
+	}
+	
+	@PUT
+	@Path("/edit")
+	public Response editSpent(@PathParam("idSpent") String idSpent) {
+		return null;
+		
+	}
+	
+	@GET
+	@Path("/find")
+	public Response findSpent(@PathParam("idSpent") String idSpent) {
+		return null;
+		
+	}
+	
+	@GET
+	@Path("/findAll")
+	public Response findAllSpents() {
+		return null;
+		
+	}
+	
+	@DELETE
+	@Path("/delete")
+	public Response deleteSpent(@PathParam("idSpent") String idSpent) {
+		return null;
+		
+	}
+}
